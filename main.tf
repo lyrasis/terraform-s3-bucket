f5ed235a5598bbb25bca66c6ae90d849e48a290d
Drew Heles <drew@codhicitta.com>
2021-05-06 16:08:07 -0400

    Add VPC endpoint

--- main.tf
+++ main.tf
@@ -54,3 +54,47 @@ resource "aws_iam_user_policy_attachment" "bucket_policy_attach" {
   user       = aws_iam_user.bucket_reader.name
   policy_arn = aws_iam_policy.bucket_policy.arn
 }
+
+data "aws_vpc" "provided" {
+  id = var.vpc_id
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id        = data.aws_vpc.provided.id
+  service_name  = "com.amazonaws.${var.region}.s3"
+
+  tags = var.tags
+}
+
+resource "aws_vpc_endpoint_route_table_association" "s3" {
+  route_table_id  = var.route_table_id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "${var.name}-VPCE-Policy"
+    Statement = [
+      {
+        Sid       = "Access-to-specific-VPCE-only"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.bucket.arn,
+          "${aws_s3_bucket.bucket.arn}/*",
+        ]
+        Condition = {
+          StringEquals = {
+            "aws:sourceVpce" = aws_vpc_endpoint.s3.id
+          }
+        }
+      },
+    ]
+  })
+}
--- variables.tf
+++ variables.tf
@@ -12,6 +12,20 @@ variable "expiration_days" {
   default     = 30
 }
 
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to which the bucket should be attached."
+  type        = string
+}
+
+variable "route_table_id" {
+  description = "The ID of the routing table with which the bucket should be associated."
+  type        = string
+}
+
 variable "tags" {
   description = "Tags to set on the bucket."
   type        = map(string)
